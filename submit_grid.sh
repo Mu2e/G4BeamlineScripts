48e1b24e9500eee7222242ef61551a3e1eda9b02
oksuzian <oksuzian>
2015-08-28 16:26:55 +0000

    Added safeguards for arguments, and switched to the default njobs=500 and events-per-job=2000000

--- submit_grid.sh
+++ submit_grid.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
+if [[ $1 != 0 && $1 != 1 && $1 != 2 ]] 
   then
     echo "No arguments supplied. Use the following options:"
     echo "Submit stage 1 jobs:   ./submit_grid.sh 0"
     echo "Submit PS jobs:        ./submit_grid.sh 1"
     echo "Submit Beam jobs:      ./submit_grid.sh 2"
+    exit
 fi
 
 cp Mu2E.in Geometry
 tar cf Geometry.tar Geometry
-command="mu2eg4bl --in=Mu2E.in --tar=Geometry.tar --njobs=2 --events-per-job=25 --g4bl-version=v2_16 --g4bl-add-args='READ_Beam_File=$1'"
+command="mu2eg4bl --in=Mu2E.in --tar=Geometry.tar --njobs=500 --events-per-job=2000000 --g4bl-version=v2_16 --g4bl-add-args='READ_Beam_File=$1'"
 echo "Executing:" $command
 echo `$command`
 now=$(date +"%m_%d_%Y")
