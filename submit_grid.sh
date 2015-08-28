5522212d1ce8e25785299c38f983fbe01a6d4562
oksuzian <oksuzian>
2015-08-28 19:44:19 +0000

    Add the defaults number of events for source file option

--- submit_grid.sh
+++ submit_grid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $1 != 0 && $1 != 1 && $1 != 2 ]] 
-  then
+then
     echo "No arguments supplied. Use the following options:"
     echo "Submit stage 1 jobs:   ./submit_grid.sh 0"
     echo "Submit PS jobs:        ./submit_grid.sh 1"
@@ -9,9 +9,15 @@ if [[ $1 != 0 && $1 != 1 && $1 != 2 ]]
     exit
 fi
 
+if [ $1 == 0 ]
+then
+    command="mu2eg4bl --in=Mu2E.in --tar=Geometry.tar --njobs=200 --events-per-job=5000 --g4bl-version=v2_16 --g4bl-add-args='READ_Beam_File=$1'"
+else
+    command="mu2eg4bl --in=Mu2E.in --tar=Geometry.tar --njobs=500 --events-per-job=2000000 --g4bl-version=v2_16 --g4bl-add-args='READ_Beam_File=$1'"
+fi
+
 cp Mu2E.in Geometry
 tar cf Geometry.tar Geometry
-command="mu2eg4bl --in=Mu2E.in --tar=Geometry.tar --njobs=500 --events-per-job=2000000 --g4bl-version=v2_16 --g4bl-add-args='READ_Beam_File=$1'"
 echo "Executing:" $command
 echo `$command`
 now=$(date +"%m_%d_%Y")
